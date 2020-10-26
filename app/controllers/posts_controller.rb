a42a19b8ad7722c4c5b3e4d7b7e2db24cbd5ca71
1210takumi <uw.t.t.t.takumi@icloud.com>
2020-10-26 17:39:34 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,10 @@
 class PostsController < ApplicationController
   def index
-
+    @posts = Post.all.order(id: "DESC")
   end
 
-  def new
-
-  end
   
   def create
-
+    redirect_to action: :index
   end
 end
